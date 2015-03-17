378777822ae24b4cbb025fc97749bd6d59ee136a
garayzuev <garayzuev@gmail.com>
2015-03-17 21:47:10 +0300

    Test run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_DIR=./simulator
+APP_DIR=./target
 APP_NAME=simulator-electric-meter-1.0-SNAPSHOT
 
 pushd $APP_DIR
@@ -11,7 +11,7 @@ mvn clean package -DskipTests=true
 
 wget ${SIMULATOR_CONFIG} -O ./configEnergy.xml
 
-java -jar ${APP_DIR}/target/${APP_NAME}.jar -nogui configEnergy.xml
+java -jar ${APP_DIR}/${APP_NAME}.jar -nogui configEnergy.xml
 
 popd
 	
